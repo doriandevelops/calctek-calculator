7b11144fda4e9137fff2a999eb84dca1bc351ac8
doriandevelops <doriandevelops@gmail.com>
2024-12-20 07:18:31 +0900

    Tailwind installed and working

--- public/js/app.js
+++ public/js/app.js
@@ -22010,8 +22010,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = {
+  "class": "text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl"
+};
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("h1", null, "Hello Calculator");
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("h1", _hoisted_1, "Hello Calculator");
 }
 
 /***/ }),
--- resources/js/components/Calculator.vue
+++ resources/js/components/Calculator.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-    <h1>Hello Calculator</h1>
+    <h1 class="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Hello Calculator</h1>
 </template>
